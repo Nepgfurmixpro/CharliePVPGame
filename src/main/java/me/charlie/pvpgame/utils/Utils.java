2efe7f7fcb2a5b21800b1caad4c253eeef3c677f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-07 22:13:37 -0500

    Updated Utils file.

--- src/main/java/me/charlie/pvpgame/utils/Utils.java
+++ src/main/java/me/charlie/pvpgame/utils/Utils.java
@@ -1,10 +1,15 @@
 package me.charlie.pvpgame.utils;
 
 import me.charlie.pvpgame.Main;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Utils {
     public static void Info(String log) {
-        System.out.println(JavaPlugin.getPlugin(Main.class).getName() + log);
+        System.out.println("[" + JavaPlugin.getPlugin(Main.class).getName() + "] " + log);
+    }
+
+    public static String Color(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
     }
 }
