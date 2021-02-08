13cfc8b3c4fe02f2969ea4f9d910bcecf145ec1c
Charrliie <57119734+Yeetukus@users.noreply.github.com>
2021-02-07 22:09:58 -0500

    Added Instance Classes

--- src/main/java/me/charlie/pvpgame/utils/Utils.java
+++ src/main/java/me/charlie/pvpgame/utils/Utils.java
@@ -4,7 +4,7 @@ import me.charlie.pvpgame.Main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Utils {
-    public static void Info(String log) {
-        System.out.println(JavaPlugin.getPlugin(Main.class).getName() + log);
-    }
+	public static void Info(String log) {
+		System.out.println(JavaPlugin.getPlugin(Main.class).getName() + log);
+	}
 }
